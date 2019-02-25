99f0ae4e4fc036dbeb5c1c14a79df9bd45453dc4
lsl1229840757 <1229840757@qq.com>
2019-02-25 14:58:41 +0800

    第四章笔记

--- programming_ex2/ex2/costFunctionReg.py
+++ programming_ex2/ex2/costFunctionReg.py
@@ -19,7 +19,7 @@ def gradient(theta, x_data, y_data, lambd):
     theta = theta.reshape((-1, 1))
     m = x_data.shape[0]
     h_x = sigmoid.sigmoid(np.dot(x_data, theta))
-    theta[0, 0] = 0
+    theta[0, 0] = 0  # don't penalize theta0
     part1 = np.dot(x_data.T, h_x-y_data)/m
     part2 = lambd/m*theta
     return (part1+part2).flatten()
